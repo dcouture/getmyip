97d718ce2b080a6235ef0de363602423c98c3b45
Don Couture <doncouture@comcast.net>
2016-03-04 00:35:33 +0000

    Version 1.0.0

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// GET home page.
+router.get('/', function(req, res) {
+// RegEx replace is used to remove ipv6 prefix Windows 7+ add to IP address
+  res.render('index', { title: 'IP Cow', ip: req.ip.replace(/^.*:/, '') })
+});
+
+module.exports = router;
